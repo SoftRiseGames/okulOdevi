eaaf8b9e4d569acce450864989ceeaacb595e6e9
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2022-12-09 23:31:43 +0300

    Initial commit

---
+++ Assets/script/Movement.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private float horizontal;
+    public Rigidbody2D rb;
+    public bool ground;
+    public bool hookground;
+    public bool hookjumped;
+    public bool afterMoveCont;
+    public bool axisanim;
+    private bool dashBool;
+    [Header("MovementSettings")]
+    [SerializeField] float speedX;
+    [SerializeField] float jumpY;
+    [SerializeField] float xRaw;
+    private float hookafterMovement;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        hookground = true;
+    }
+
+    // Update is called once per frame
+   
+    private void FixedUpdate()
+    {
+        //exenKontroller;
+        xRaw = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxis("Horizontal");
+        
+        //afterHook
+        if (hookground && hookjumped && !ground)
+            rb.velocity = new Vector2(5 * hookafterMovement, rb.velocity.y);
+        
+        //normal kontroller
+
+        else if (hookground && !hookjumped)
+            rb.velocity = new Vector2(speedX * horizontal, rb.velocity.y);
+
+        Jump();
+        hookExens();
+        Dash(xRaw);
+       
+      
+    }
+    
+    void Update()
+    {
+        charExens(); 
+    }
+    
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "zemin")
+            ground = true; hookjumped = false; dashBool = true;
+        axisanim = true;
+    }
+    public void Dash(float x)
+    {
+        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (!ground && dashBool)
+            {
+                rb.velocity = Vector2.zero;
+                rb.velocity = new Vector2(x, rb.velocity.y).normalized * 300;
+                dashBool = false;
+            }
+        }
+
+       
+    }
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.W) && ground)
+        {
+            ground = false;
+            rb.velocity = new Vector2(rb.velocity.x, jumpY);
+        }
+    }
+    void hookExens()
+    {
+        if (this.gameObject.transform.localScale.x >= 0)
+            hookafterMovement = 1;
+        else
+            hookafterMovement = -1;
+    }
+    void charExens()
+    {
+        if (axisanim)
+        {
+            if (horizontal < 0 && this.gameObject.transform.localScale.x >= 0)
+                this.gameObject.transform.localScale = new Vector2(this.gameObject.transform.localScale.x * -1, this.gameObject.transform.localScale.y);
+            else if (horizontal > 0 && this.gameObject.transform.localScale.x < 0)
+                this.gameObject.transform.localScale = new Vector2(this.gameObject.transform.localScale.x * -1, this.gameObject.transform.localScale.y);
+        }
+    }
+}
---
+++ Assets/script/kancaSistemi.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kancaSistemi : MonoBehaviour
+{
+    [SerializeField] private Transform hedefPozisyon;
+    [SerializeField] private LineRenderer lr;
+    [SerializeField] private DistanceJoint2D dj;
+    
+    private Node selectednode;
+    public static kancaSistemi instance;
+    public Movement moveCont;
+    
+    bool asilikalma;
+    public LayerMask layercontrol;
+    void Start()
+    {
+        lr.enabled = false;
+        dj.enabled = false;
+        selectednode = null;
+        moveCont = GameObject.Find("karakter").GetComponent<Movement>();
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(selectednode == null)
+        {
+            lr.enabled = false;
+            dj.enabled = false;
+            return;
+        }
+
+        lr.enabled = true;
+        dj.enabled = true;
+
+        dj.connectedBody = selectednode.GetComponent<Rigidbody2D>();
+        if(selectednode != null)
+        {
+            lr.SetPosition(0, transform.position);
+            lr.SetPosition(1, selectednode.transform.position);
+        }
+
+        if (!moveCont.hookground)
+        {
+            kancascale(selectednode);
+        }
+    
+    }
+    public void SelectNode(Node node)
+    {
+        selectednode = node;
+        moveCont.axisanim = false;
+        moveCont.hookground = false;
+        moveCont.hookjumped = false;
+        this.gameObject.transform.parent = selectednode.transform;
+        Debug.Log(this.gameObject.transform.position.x);
+      
+    }
+    public void Deselect()
+    {
+        moveCont.axisanim = true;
+        selectednode = null;
+        this.gameObject.transform.parent = null;
+        moveCont.hookground = true;
+        moveCont.hookjumped = true;
+    }
+    public void kancascale(Node node)
+    {
+        /*
+        if (this.gameObject.transform.position.x > node.gameObject.transform.position.x && this.gameObject.transform.localScale.x < 0f)
+            this.gameObject.transform.localScale = new Vector2(this.gameObject.transform.localScale.x * -1, this.gameObject.transform.localScale.y);
+        else if (this.gameObject.transform.position.x <node.gameObject.transform.position.x && this.gameObject.transform.localScale.x >= 0f)
+            this.gameObject.transform.localScale = new Vector2(this.gameObject.transform.localScale.x * -1, this.gameObject.transform.localScale.y);
+    */  
+    }
+}
