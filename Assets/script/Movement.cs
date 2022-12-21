f6cb1b942b8dcb41c8a1bacc3dc988f84f369c45
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2022-12-21 20:59:44 +0300

    LastCommit

--- Assets/script/Movement.cs
+++ Assets/script/Movement.cs
@@ -57,8 +57,12 @@ public class Movement : MonoBehaviour
     private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "zemin")
-            ground = true; hookjumped = false; dashBool = true;
+            hookjumped = false; dashBool = true;
         axisanim = true;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        ground = true;
     }
 
     void Jump()
