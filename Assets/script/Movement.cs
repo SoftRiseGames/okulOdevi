b76b6bdd7d9498ad4952598a0ee25a28e3686b36
SoftRiseGames <98230446+SoftRiseGames@users.noreply.github.com>
2023-01-09 18:27:39 +0300

    Update Movement.cs

--- Assets/script/Movement.cs
+++ Assets/script/Movement.cs
@@ -97,7 +97,6 @@ public class Movement : MonoBehaviour
         {
             if (rb.velocity.x < 0)
             {
-
                 this.gameObject.transform.localScale = new Vector2(-1.5f, this.gameObject.transform.localScale.y);
                 Debug.Log("left");
 
